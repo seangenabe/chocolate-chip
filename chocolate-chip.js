96350e0e47c4c6b973ad18aac5a00e50147a0606
Matt Perry <MattPerry@Matts-MacBook-Pro.local>
2015-06-18 18:11:51 +0100

    added set function

--- chocolate-chip.js
+++ chocolate-chip.js
@@ -1,7 +1,20 @@
 "use strict";
 
 var SEPERATOR = '; ',
-	EQUALS = '=';
+	EQUALS = '=',
+
+	encode = encodeURIComponent,
+	decode = decodeURIComponent,
+
+	paramString = function (name, value) {
+		var param = SEPERATOR + name;
+
+		if (value !== undefined) {
+			param += EQUALS + value;
+		}
+
+		return param;
+	};
 
 module.exports = {
 
@@ -21,12 +34,11 @@ module.exports = {
 			cookieKeyValue = allCookies[i].split(EQUALS);
 
 			// Check if cookie key is the same as provided name
-			if (decodeURIComponent(cookieKeyValue[0]) === name) {
-				return decodeURIComponent(cookieKeyValue[1]);
+			if (decode(cookieKeyValue[0]) === name) {
+				return decode(cookieKeyValue[1]);
 			}
 		}
 	},
-
 
 	/*
 		Set cookie value
@@ -42,6 +54,47 @@ module.exports = {
 		@return: this
 	*/
 	set: function (name, value, opts) {
+		var cookie = encode(name) + EQUALS + encode(value),
+			expires = '',
+			end;
+
+		opts = opts || {};
+		end = opts.end;
+
+		if (end) {
+			switch (typeof end) {
+				case 'number':
+					// If this number is set to Infinity, set to arbitary time
+					if (end === Infinity) {
+						expires = paramString('expires', 'Fri, 31 Dec 9999 23:59:59 GMT');
+
+					// Or if it's in ms, set max-age
+					} else {
+						expires = paramString('max-age', end);
+					}
+					break;
+
+				// Assume this is a time string
+				case 'string':
+					expires = paramString('expires', end);
+					break;
+
+				// Assume this is a Date object
+				case 'object':
+					if (end.toUTCString) {
+						expires = paramString('expires', end.toUTCString());
+					}
+					break;
+			}
+		}
+
+		// Build cookie string
+		cookie += expires;
+		cookie += (opts.path) ? paramString('path', opts.path) : '';
+		cookie += (opts.domain) ? paramString('domain', opts.domain) : '';
+		cookie += (opts.secure) ? paramString('secure') : '';
+
+		document.cookie = cookie;
 
 		return this;
 	},
